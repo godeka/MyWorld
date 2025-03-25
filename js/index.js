66b7e114b50c303f4d51c82cb2049e88191efb7b
godeka <godeka@naver.com>
2025-03-25 20:40:39 +0900

    fix: 줌 제거하여 지도 완전히 고정 #4

--- js/index.js
+++ js/index.js
@@ -5,12 +5,16 @@ mapboxgl.accessToken =
 const map = new mapboxgl.Map({
   container: "map",
   center: [0, 0], // 경도, 위도
-  zoom: 1,
+  zoom: 1.5,
   style: "mapbox://styles/mapbox/light-v11",
   projection: "equirectangular", // 직사각형 투영
 });
 map.setRenderWorldCopies(false); // 루프 방지
 map.dragPan.disable(); // 드래그 방지
+// 줌 제거
+map.scrollZoom.disable();
+map.boxZoom.disable();
+map.doubleClickZoom.disable();
 
 fetch("./data/countries_ko.json")
   .then((res) => res.json())
