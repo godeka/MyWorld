36b8be6ab1bdc561730a367bdf2f3dccf2e48b8b
godeka <godeka@naver.com>
2025-04-05 08:28:15 +0900

    chore: add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production", // development
+  mode: "development", // production or development
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname), // index.html 파일이 있는 위치
+    },
   },
 };
