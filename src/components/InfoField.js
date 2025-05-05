de41adb12954d14c952c288d7ade51b526b8bc83
godeka <godeka@naver.com>
2025-05-05 17:13:44 +0900

    design: design info field #6

--- src/components/InfoField.js
+++ src/components/InfoField.js
@@ -15,10 +15,12 @@ export default function InfoField({ $app, initialState }) {
     const numVisited = this.state.selectedCountries.length;
     const percentage = Math.round((numVisited / 249) * 100 * 100) / 100;
     const $info = document.querySelector(".info-container");
-    $info.textContent =
+
+    const highlight = `<span class="info-highlight">${percentage}% (${numVisited}/249)</span>`;
+    $info.innerHTML =
       this.state.lang === "ko"
-        ? `당신이 가본 나라는 전 세계의 ${percentage}% (${numVisited}/249) 입니다.`
-        : `You've visited ${percentage}% (${numVisited}/249) of the countries in the world.`;
+        ? `당신이 가본 나라는 전 세계의 ${highlight} 입니다.`
+        : `You've visited ${highlight} of the countries in the world.`;
   };
 
   this.render();
